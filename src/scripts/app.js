bb1aca541f60b3c99e68f7c08611679d5c492c97
kesleymartins <kesley.martins@vivaldi.net>
2023-08-15 15:49:13 -0300

    Refactor

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,54 +1,11 @@
-const storagePrefix = "ghost"
+import { HostsStore } from './hostsStore.js'
+import { HostsComponent } from './hostsComponent.js'
+import { TabsComponent } from './tabsComponent.js'
 
-// Data Setup
-let data = localStorage.getItem(`${storagePrefix}_data`)
+const hostsStore = new HostsStore()
+const hostsComponent = new HostsComponent()
+const tabsComponent = new TabsComponent()
 
-if (data) {
-    data = JSON.parse(data)
-} else {
-    default_data = {
-        "links": [] 
-    }
-
-    localStorage.setItem(`${storagePrefix}_data`, JSON.stringify(default_data))
-    data = default_data
-}
-
-// Set loaded links
-const contentSection = document.querySelector('section#content')
-
-data.links.forEach(link => {
-    const element = document.createElement('a')
-    
-    element.setAttribute('id', link.id)
-    element.setAttribute('href', link.url)
-    element.innerText = link.name
-
-    contentSection.appendChild(element)
-});
-
-// Form Logic
-const formSection = document.querySelector('section#form')
-const formToggleButton = formSection.querySelector('button#toggler')
-const form = formSection.querySelector('form')
-
-formToggleButton.addEventListener('click', event => {
-    event.preventDefault()
-
-    form.classList.toggle('hide')
-})
-
-form.addEventListener('submit', event => {
-    event.preventDefault()
-
-    const nameInput = form.querySelector('input#name')
-    const urlInput = form.querySelector('input#url')
-
-    data.links.push({
-        id: data.links.length + 1,
-        name: nameInput.value,
-        url: urlInput.value
-    })
-
-    localStorage.setItem(`${storagePrefix}_data`, JSON.stringify(data))
-})
+hostsStore.fetchHosts()
+hostsComponent.mount(hostsStore.data)
+tabsComponent.mount()
