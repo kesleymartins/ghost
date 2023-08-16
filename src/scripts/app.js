0e1d224db86463af4d15ed4c210e2e82e74659f7
kesleymartins <kesley.martins@vivaldi.net>
2023-08-16 00:27:15 -0300

    improve files organization

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,13 +1,13 @@
-import { HostsStore } from './hostsStore.js'
-import { HostsComponent } from './hostsComponent.js'
-import { TabsComponent } from './tabsComponent.js'
-import { FormComponent } from './formComponent.js'
+import { HostsStore } from './stores/hostsStore.js'
+import { HostsComponent } from './components/hostsComponent.js'
+import { TabsComponent } from './components/tabsComponent.js'
+import { FormComponent } from './components/formComponent.js'
 
 const hostsStore = new HostsStore()
 const tabsComponent = new TabsComponent()
 const hostsComponent = new HostsComponent()
 const formComponent = new FormComponent()
 
-tabsComponent.mount(hostsStore)
+tabsComponent.mount(hostsStore, hostsComponent)
 hostsComponent.mount(hostsStore)
 formComponent.mount(hostsStore)
