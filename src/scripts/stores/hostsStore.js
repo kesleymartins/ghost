be98a483bfc229e91aa06e9999f8cff0a21764ea
kesleymartins <kesley.martins@vivaldi.net>
2023-08-18 05:03:48 -0300

    add JSDocs and encapsulate

--- src/scripts/stores/hostsStore.js
+++ src/scripts/stores/hostsStore.js
@@ -1,29 +1,42 @@
-function HostsStore() {
-  this.storageName = "ghost_hosts"
-  this.data = [] 
+import '../utils/types.js'
 
+/**
+ * Store para lidar com o CRUD de Host
+ */
+function HostsStore() {
+  this.data = [] 
+  const storageName = "ghost_hosts"
+  
+  /**
+   * Atualiza os dados locais com os do localStorage
+   */
   this.fetchHosts = () => {
-    const data = JSON.parse(localStorage.getItem(this.storageName))
+    const data = JSON.parse(localStorage.getItem(storageName))
     this.data = data ? data : []
   } 
 
+  /**
+   * Adiciona um host no localStorage
+   *
+   * @param {Host} newHost
+   */
   this.addHost = (newHost) => {
     const id = this.data.length
     newHost = { id, ...newHost }
 
     this.data.push(newHost)
-    localStorage.setItem(this.storageName, JSON.stringify(this.data))
+    localStorage.setItem(storageName, JSON.stringify(this.data))
   }
   
   /**
-    * Função para remover um host
-    * 
-    * @param {number} hostId
-    */
+   * Remove um Host pelo Id 
+   * 
+   * @param {number} hostId
+   */
   this.removeHost = (hostId) => {
     this.fetchHosts()
     this.data = this.data.filter(host => host.id !== hostId)
-    localStorage.setItem(this.storageName, JSON.stringify(this.data))
+    localStorage.setItem(storageName, JSON.stringify(this.data))
   }
 }
 
