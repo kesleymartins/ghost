6332ac6dbfd2e5a7a335218769a4a5f632add310
kesleymartins <kesley.martins@vivaldi.net>
2023-08-18 04:51:25 -0300

    create Host type

---
+++ src/scripts/utils/types.js
@@ -0,0 +1,6 @@
+/**
+ * @typedef {object} Host   
+ * @property {int | undefined} id   - Identificador único do Host
+ * @property {string} name          - Nome do host
+ * @property {string} url           - Url de redirecionamento do Host
+ */
