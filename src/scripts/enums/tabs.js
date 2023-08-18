fd23b130e4a5cf30697e965fbb5892bb92a8a61b
kesleymartins <kesley.martins@vivaldi.net>
2023-08-18 20:48:29 -0300

    document tabs enum

--- src/scripts/enums/tabs.js
+++ src/scripts/enums/tabs.js
@@ -1,5 +1,22 @@
+/**
+ * Representa as abas disponiveis para seleção
+ *  
+ * @enum {string}
+ */
 const Tabs = {
+
+  /**
+   * Aba para a listagem de hosts
+   *
+   * @type {string}
+   */
   HOSTS: 'hosts',
+  
+  /**
+   * Aba de formulário
+   *
+   * @type {string}
+   */
   FORM: 'form'
 }
 
